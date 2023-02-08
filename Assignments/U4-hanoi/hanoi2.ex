24684ae1d135fa74203b1542f9a2a8ab70685604
Björn Formgren <formgrenbjorn@gmail.com>
2023-02-08 08:15:09 +0100

    Assignment U4 - Towers of Hanoi
    Solved the recursive problem Towers of Hanoi

---
+++ Assignments/U4-hanoi/hanoi2.ex
@@ -0,0 +1,11 @@
+defmodule Hanoi do
+  def solve(n, from, to, auxiliary) when n > 0 do
+    solve(n - 1, from, auxiliary, to)
+    IO.puts("Move disk from #{from} to #{to}")
+    solve(n - 1, auxiliary, to, from)
+  end
+
+  def solve(_, _, _, _), do: nil
+end
+
+# Hanoi.solve(5, :left, :right, :center)
